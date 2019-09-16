a7666c48b92b9bbd22cbe1a4c4930605b8921638
d.afonchenko <d.afonchenko@dodopizza.com>
2019-09-16 18:10:27 +0300

    Add base square

--- Assets/Scripts/Alchemy.cs
+++ Assets/Scripts/Alchemy.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Alchemy : MonoBehaviour
 {
+    public GameObject prefabType;
+    public Canvas canvas;
+    
+    public ElementData[] elements = new ElementData[]
+    {
+        new ElementData() {color = Color.red},
+        new ElementData() {color = Color.blue},
+    };
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var element = Instantiate(prefabType, canvas.transform);
+        element.GetComponent<Image>().color = elements[1].color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
+}
+
+[Serializable]
+public class ElementData
+{
+    public Color color;
 }
