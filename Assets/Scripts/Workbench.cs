341ebdbdd699988483ce79ecb1a469e3372b987c
d.afonchenko <d.afonchenko@dodopizza.com>
2019-09-19 17:36:05 +0300

    Add maximum element count

--- Assets/Scripts/Workbench.cs
+++ Assets/Scripts/Workbench.cs
@@ -1,13 +1,22 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Workbench : MonoBehaviour
 {
+    public int elementsMaximum = 2;
+    public List<Element> currentElements = new List<Element>();
     public GameObject elementPrefabType;
     
     public void AddElement(ElementData elementData)
     {
+        if (currentElements.Count >= elementsMaximum)
+        {
+            return;
+        }
+        
         var elementObject = Instantiate(elementPrefabType, transform);
-
-        elementObject.GetComponent<Element>().Init(elementData);
+        var element = elementObject.GetComponent<Element>();
+        element.Init(elementData);
+        currentElements.Add(element);
     }
 }
