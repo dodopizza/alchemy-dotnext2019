3e9fcf258669e7bc220362e4a5bf6465c64082ee
d.afonchenko <d.afonchenko@dodopizza.com>
2019-09-20 11:21:17 +0300

    Add check for empty mix

--- Assets/Scripts/ElementMixer/LocalElementMixer.cs
+++ Assets/Scripts/ElementMixer/LocalElementMixer.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace ElementMixer
@@ -7,6 +8,11 @@ namespace ElementMixer
     {
         public ElementData MixElements(IEnumerable<ElementData> elementsData)
         {
+            if (!elementsData.Any())
+            {
+                return null;
+            }
+            
             return new ElementData
             {
                 color = new Color(0.5f, 0.1f, 0.4f)
