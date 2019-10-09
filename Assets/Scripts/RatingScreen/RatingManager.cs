edcd5bca15febb51170b6764f413d5eadb406218
Dmitry Pavlushin <d.pavlushin@dodopizza.com>
2019-10-09 17:28:15 +0300

    Log ratings to debug

--- Assets/Scripts/RatingScreen/RatingManager.cs
+++ Assets/Scripts/RatingScreen/RatingManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Domain;
+using Domain.Models;
+using UnityEngine;
 
 namespace RatingScreen
 {
@@ -7,7 +9,38 @@ namespace RatingScreen
         // Start is called before the first frame update
         void Start()
         {
-        
+            var ratingFetcher = new DummyRatingFetcher();
+
+            ratingFetcher.GetRating()
+                .ContinueWith(task =>
+                {
+                    if (task.IsCanceled)
+                    {
+                        // TODO error
+                    }
+
+                    if (task.IsFaulted)
+                    {
+                        // TODO error
+                    }
+                    
+                    var result = task.Result;
+
+                    if (!result.IsSuccess)
+                    {
+                        // TODO error
+                    }
+                    
+                    RenderRatings(task.Result.Data.Top);
+                });
+        }
+
+        void RenderRatings(RatingEntry[] ratings)
+        {
+            foreach (var entry in ratings)
+            {
+                Debug.Log($"{entry.Nickname}: {entry.Rating}");
+            }
         }
 
         // Update is called once per frame
