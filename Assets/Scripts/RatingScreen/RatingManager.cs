7742a75643c6da6b2fd778f1b83b92dbbbbf7d46
Dmitry Pavlushin <d.pavlushin@dodopizza.com>
2019-10-09 17:10:22 +0300

    Move rating manager to proper namespace

--- Assets/Scripts/RatingScreen/RatingManager.cs
+++ Assets/Scripts/RatingScreen/RatingManager.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class RatingManager : MonoBehaviour
+namespace RatingScreen
 {
-    // Start is called before the first frame update
-    void Start()
+    public class RatingManager : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
         
+        }
     }
 }
