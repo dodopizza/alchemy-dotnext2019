29420382ef6d05b310a1d60cfe0c54b8ecc119b2
Evgeny Bikkinin <e.bikkinin@dodopizza.com>
2019-10-26 19:39:25 +0300

    Add description on long tap

--- Assets/Scripts/MainScreen/BookElementItem.cs
+++ Assets/Scripts/MainScreen/BookElementItem.cs
@@ -6,18 +6,21 @@ using UnityEngine.UI;
 
 namespace MainScreen
 {
-    public class BookElementItem : MonoBehaviour, IPointerClickHandler
+    public class BookElementItem : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
     {
         public GameObject floatingElementPrefab;
 
         private Image _image;
         private Element _element;
-        readonly GameManager _gameManager = GameManager.Instance;
+        private readonly GameManager _gameManager = GameManager.Instance;
+        private bool _held;
+        private GameObject _newElementWindowPrefab;
 
         private void Start()
         {
             _image = GetComponentInChildren<Image>();
             _image.sprite = _element.Sprite;
+            _newElementWindowPrefab = (GameObject) Resources.Load("Prefabs/NewElementWindow", typeof(GameObject));
 
             GetComponentInChildren<Text>().text = _element.Name;
         }
@@ -40,13 +43,41 @@ namespace MainScreen
 
         public async void OnPointerClick(PointerEventData eventData)
         {
-            if (!_gameManager.CheckAndLockInput()) 
+            Debug.Log("OnPointerClick");
+
+            if (!_gameManager.CheckAndLockInput() || _held) 
                 return;
 
             _gameManager.AddElementToForge(_element);
             
             var mixElement = _gameManager.EmptyForgeSlot;
             await _gameManager.HandleUiOperation(SendElementToForge(mixElement));
+        }
+        
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            _held = false;
+            Invoke(nameof(OnLongPress), 1);
+        }
+ 
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            CancelInvoke(nameof(OnLongPress));
+        }
+ 
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            CancelInvoke(nameof(OnLongPress));
+        }
+ 
+        private async Task OnLongPress()
+        {
+            _held = true;
+            var newElementWindow = Instantiate(_newElementWindowPrefab, _gameManager.UnderUpperLayerTransform)
+                .GetComponent<NewElementWindow>();
+                                        
+            newElementWindow.Initialize(_element.Sprite, _element.Name, _element.Score, _element.Description);
+            await newElementWindow.Show();
         }
     }
 }
